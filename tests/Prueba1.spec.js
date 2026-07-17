2ce80f29a1bded728dbafc781bb7a06c5c7916c8
Katerine Rodriguez <katerodri.05@gmail.com>
2026-07-17 11:39:24 -0600

    Primer commit

---
+++ tests/Prueba1.spec.js
@@ -0,0 +1,16 @@
+const {test, expect} = require('@playwright/test')
+
+
+
+//test('My first test', async ({page}) => {
+
+  // await page.goto('https://google.com')
+   //await expect(page).toHaveTitle(/Google/)
+//})
+
+
+
+
+//const {hello, world} = require('./Demo/Hello')
+//console.log (hello());
+//console.log (world());
